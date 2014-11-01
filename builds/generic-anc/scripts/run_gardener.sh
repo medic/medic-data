7a5c8e389b9e6145a2704ee504fa35d163b8d03e
Milan Andric <milan@medicmobile.org>
2014-11-01 11:16:02 -0200

    debugging gardener

--- builds/generic-anc/scripts/run_gardener.sh
+++ builds/generic-anc/scripts/run_gardener.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 DEMOS_COUCHDB=${DEMOS_COUCHDB:-http://localhost:5984}
-
 export COUCH_URL="${DEMOS_COUCHDB}/medic"
 echo "COUCH_URL is ${COUCH_URL}"
 gardener "${DEMOS_COUCHDB}/medic" &
