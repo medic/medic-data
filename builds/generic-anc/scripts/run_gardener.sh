4a841e7a10335b944d63aae4dae7d1972aab5bf4
Milan Andric <milan@medicmobile.org>
2014-11-05 19:52:35 -0200

    manage gardener process better.

--- builds/generic-anc/scripts/run_gardener.sh
+++ builds/generic-anc/scripts/run_gardener.sh
@@ -2,5 +2,6 @@
 
 DEMOS_COUCHDB=${DEMOS_COUCHDB:-http://localhost:5984}
 export COUCH_URL="${DEMOS_COUCHDB}/medic"
-echo "COUCH_URL is ${COUCH_URL}"
 gardener "${DEMOS_COUCHDB}/medic" &
+echo "$!" > gardener.PID
+echo "gardener PID `cat gardener.PID`"
